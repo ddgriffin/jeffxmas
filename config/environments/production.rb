a1c0386d06a16642f0e9392afa30353828b96cdd
ddgriffin <dianadgriffin@gmail.com>
2014-12-30 16:52:22 -0600

    cleanup after getting emails working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,19 +61,6 @@ Jeffxmas::Application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password: ENV["GMAIL_PASSWORD"]
   }
-
-# ActionMailer::Base.smtp_settings = {
-#   :address        => 'smtp.sendgrid.net',
-#   :port           => '587',
-#   :authentication => :plain,
-#   :user_name      => ENV['SENDGRID_USERNAME'],
-#   :password       => ENV['SENDGRID_PASSWORD'],
-#   :domain         => 'heroku.com',
-#   :enable_starttls_auto => true
-# }
-
-
-
   config.action_mailer.perform_deliveries = true
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = false
